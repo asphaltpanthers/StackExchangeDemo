3e876fce90f6880dc5099ee212857eaf71d3f918
diser <diser@asphaltpanthers.com>
2018-06-12 22:46:38 -0400

    Add get method to Http.

--- SED.Platform/Http.cs
+++ SED.Platform/Http.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,28 @@ namespace SED.Platform
 {
     public class Http
     {
+        private HttpClient _http;
+
+        public Http(string baseAddress)
+        {
+            _http = new HttpClient()
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+        }
+
+        public T Get<T>(string url)
+        {
+            var result = _http.GetAsync(url).Result;
+            var payload = result.Content.ReadAsStringAsync().Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<T>(payload);
+            }
+            else
+            {
+                throw new Exception(payload);
+            }
+        }
     }
 }
